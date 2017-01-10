c538807c56dccf8cc18c9e5a4018d54fc1ad5845
kuwako <m.kuwako0702@gmail.com>
2017-01-10 12:06:25 +0000

    server起動処理変更

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var http = require("http");
 var settings = require("./settings");
 console.log(settings);
+console.log(settings.port);
+console.log(settings.host);
 var server = http.createServer();
 
 server.on('request', function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('hello world!!!');
+    res.write('hello from ' + req.url);
     res.end();
 });
 
