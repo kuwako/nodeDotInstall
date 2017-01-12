3821e6ebb54eda3df168fa3fe7f52ea037207463
kuwako <m.kuwako0702@gmail.com>
2017-01-12 13:32:36 +0000

    htmlファイル読み込み

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var http = require("http");
+var http = require("http"), 
+    fs = require("fs");
 var settings = require("./settings");
 console.log(settings);
 console.log(settings.port);
@@ -6,9 +7,16 @@ console.log(settings.host);
 var server = http.createServer();
 
 server.on('request', function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('hello from ' + req.url);
-    res.end();
+    fs.readFile(__dirname + '/public_html/hello.html', 'utf-8', function(err, data) {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.write('not found');
+            return res.end();
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    });
 });
 
 server.listen(settings.port, settings.host);
