c3af5f6f4596634d7f0d8db97472e298e87a5071
kuwako <m.kuwako0702@gmail.com>
2017-01-18 13:09:48 +0000

    MongoDb 接続

---
+++ mongo.js
@@ -0,0 +1,6 @@
+var MongoClient = require('mongodb').MongoClient,
+    settings = require('./settings');
+MongoClient.connect("mongodb://localhost/" + settings.db, function(err, db) {
+    if (err) { return console.dir(err); }
+    console.log("connected to db");
+});
