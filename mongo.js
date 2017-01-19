8231e4974f12c9a530d01c980925ebd1a040c879
kuwako <m.kuwako0702@gmail.com>
2017-01-19 09:50:26 +0000

    DBへの挿入と表示

--- mongo.js
+++ mongo.js
@@ -3,4 +3,24 @@ var MongoClient = require('mongodb').MongoClient,
 MongoClient.connect("mongodb://localhost/" + settings.db, function(err, db) {
     if (err) { return console.dir(err); }
     console.log("connected to db");
+
+    db.collection("users", function(err, collection){
+        var docs = [
+            {name: "taguchi", score: 40},
+            {name: "kuwako", score: 60},
+            {name: "masaki", score: 80},
+        ];
+        
+        // collection.find({name: "kuwako"}).toArray(function(err, items) {
+        //     console.log(items)
+        // });
+        var stream = collection.find().stream();
+        stream.on("data", function(item) {
+            console.log(item)
+        });
+
+        stream.on("end", function() {
+            console.log("finished");
+        });
+    });
 });
